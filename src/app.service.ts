7c88da131881e40eca1f3a8db3d1366d22c16cab
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-09 13:48:03 -0600

    AuthMean: Inicio de backend - NestBackend

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,9 @@
+// 1. Instalar globalmente la aplicación: npm i -g @nestjs/cli
+// 2. Crear la aplicación nest: nest new nest-backend
+// 3. Desinstalar con: npm uninstall prettier eslint-config-prettier eslint-plugin-prettier
+// 4. Con el panel de control recargar la ventana
+// 5. Ejecutar la aplicación npm run start:dev
+// 6. Postman enviar un get send a la url http://localhost:3000/
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
