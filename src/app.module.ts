f9b61bde6df044a8b1b4d51ec1e091b036ad8340
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-09 20:01:24 -0600

    AuthMean: Conectar Nest con MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-// 12. Generar un recurso completo de autenticación con comando nest g resource auth
-// 13. Seleccionar REST API
+// 12. Ver documentación en https://docs.nestjs.com/techniques/mongodb
+// 13. Instalar las dependencias npm i @nestjs/mongoose mongoose
+// 14. Agregar en este archivo MongooseModule.forRoot('mongodb://localhost/nest') en los imports
+// 15. Cambiar el numero del puerto
+// 16. Crear archivo .env y agregar la url de mongo
+// 17. Agregar la terminación del archivo .env al gitignore
+// 18. Crear otro archivo .env.template
+// 19. El readme borrarlo y dejar las instrucciones para levantar app
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017')],
   controllers: [],
   providers: [],
 })
