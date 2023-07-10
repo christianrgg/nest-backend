926e89054fdfe9d2238d6b3c04bcb7681ff8c380
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-09 20:38:49 -0600

    AuthMean: Variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
-// 12. Ver documentación en https://docs.nestjs.com/techniques/mongodb
-// 13. Instalar las dependencias npm i @nestjs/mongoose mongoose
-// 14. Agregar en este archivo MongooseModule.forRoot('mongodb://localhost/nest') en los imports
-// 15. Cambiar el numero del puerto
-// 16. Crear archivo .env y agregar la url de mongo
-// 17. Agregar la terminación del archivo .env al gitignore
-// 18. Crear otro archivo .env.template
-// 19. El readme borrarlo y dejar las instrucciones para levantar app
+// 20. Para las variables de entorno instalar npm i @nestjs/config
+// 21. Agregar a los imports ConfigModule.forRoot()
+// 22. Crear constructor e imprimir el proceso .env constructor(){ console.log(process.env);} ver en consola corriendo mongo_url
+// 23. Cambiar la liga de conexion por el proceso .env . mongo
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    AuthModule,
-    MongooseModule.forRoot('mongodb://localhost:27017')],
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    AuthModule
+  ],  
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  
+}
